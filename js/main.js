e50e03d794eac70cc9ba19e532d973ff8f8cb198
Oliver <sex.xiaoyee@gmail.com>
2018-03-23 01:44:20 +0800

    finish index.html

--- js/main.js
+++ js/main.js
@@ -10,4 +10,29 @@ $(function () {
       $li.find('.hover-menus-wrapper').slideUp();
     });
 
+
+  /**
+   * 设置 service 垂直剧中显示
+   */
+  function setServiceItemsPositioin() {
+    var $serviceList = $('.service-list');
+    var height = $serviceList.height();
+
+    var $abContentWrapper = $('.ab-content-wrapper');
+    var abHeight = $abContentWrapper.find('.container').height();
+
+    var gap = (height - abHeight) / 2;
+    $abContentWrapper.css({top: gap});
+  }
+
+  setServiceItemsPositioin();
+
+  /**
+   * 每次窗口大小改变
+   */
+  $(window).resize(function () {
+    console.log('resize');
+    setServiceItemsPositioin();
+  })
+
 });
