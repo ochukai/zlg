94fba1f6d959e01f5301ac8291968d83a6dd6d38
Oliver <sex.xiaoyee@gmail.com>
2018-03-27 02:03:10 +0800

    may be last change

--- js/main.js
+++ js/main.js
@@ -55,42 +55,14 @@ $(function () {
     changeArrow();
   });
 
-
-//
-
-  if(BaiduMap) {
-
-    var map = new BaiduMap({
-      id: "container3",
-      level: 16, //  选填--地图级别--(默认15)
-      zoom: true, // 选填--是否启用鼠标滚轮缩放功能--(默认false)
-      type: [ "地图", "卫星", "三维" ], // 选填--显示地图类型--(默认不显示)
-      width: 320, // 选填--信息窗口width--(默认自动调整)
-      height: 70, // 选填--信息窗口height--(默认自动调整)
-      titleClass: "title",
-      contentClass: "content",
-      showPanorama: true, // 是否显示全景控件(默认false)
-      showMarkPanorama: true, // 是否显示标注点全景图(默认false)
-      showLabel: false, // 是否显示文本标注(默认false)
-      mapStyle: "normal", // 默认normal,可选dark,light
-      icon: { // 选填--自定义icon图标
-        url: "img/marker2.png",
-        width: 34,
-        height: 94
-      },
-      centerPoint: { // 中心点经纬度
-        lng: 118.106586,
-        lat: 24.467207
-      },
-      index: 3, // 开启对应的信息窗口，默认-1不开启
-      animate: true, // 是否开启坠落动画，默认false
-      points: {
-        lng: "116°23′",
-        lat: "39°54′"
-      }, // 标注点--id(唯一标识)
-      callback: function (id) { // 点击标注点回调
-        $(".list").find("li").eq(id - 1).addClass("active").siblings().removeClass("active");
-      }
-    });
-  }
+  
+//  landing pic change
+  $('.carousel').carousel({
+    interval: 5000
+  });
+  
+  $('.landing-pic .small-bar').on('click', function () {
+    console.log('next picture');
+    $('.carousel').carousel('next');
+  })
 });
